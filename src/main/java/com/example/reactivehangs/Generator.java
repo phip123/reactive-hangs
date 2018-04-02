62625866e8255590eaa928ed95e4e145c31e9aaf
phip123 <poepsi39@gmail.com>
2018-04-02 12:54:53 +0200

    saveAll  hangs up

--- src/main/java/com/example/reactivehangs/Generator.java
+++ src/main/java/com/example/reactivehangs/Generator.java
@@ -5,6 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class Generator {
@@ -16,9 +19,18 @@ public class Generator {
         this.personRepository = personRepository;
     }
 
-    @PostConstruct
     public void setup() {
         personRepository.deleteAll().block();
         LOG.info("Never happens");
+    }
+
+    @PostConstruct
+    public void setup2() {
+        List<Person> personList = new LinkedList<>();
+        for (int i = 0; i < 200; i++) {
+            personList.add(Person.PersonBuilder.aPerson().uuid(UUID.randomUUID()).name("Name " + i).build());
+        }
+
+        personRepository.saveAll(personList).blockLast();
     }
 }
